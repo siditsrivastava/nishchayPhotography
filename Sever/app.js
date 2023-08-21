7b5a9516b39ccdf86550b2ffce70ef2e1769ff85
Sidit Srivastava <siditsrivastava84@gmail.com>
2023-08-21 20:57:30 +0530

    updatedFiles

--- Sever/app.js
+++ Sever/app.js
@@ -3,29 +3,16 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 const dotenv = require("dotenv");
-const compression = require("compression");
 
 dotenv.config({
   path: "./config.env",
 });
 app.use(cors());
-app.use(compression({
-  level:6,
-  threshold:100*10000,
-  filter:(req, res) => {
-    if(req.header['x-no-compression']){
-      return false
-    }
-    return compression.filter(req,res);
-  }
-}));
+
 app.use(bodyParser.json());
 app.use(require("./route"));
 app.use(express.json());
 console.log("sidit Srivastava");
 app.listen(4000, () => {
   console.log("sever runn in the localhost 4000");
-});
-app.get("/", (req, res) => {
-  res.send("hello");
 });
